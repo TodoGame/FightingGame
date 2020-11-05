9ecd7b7f8ed5e55a8e9f9c9becb49e04ee5a7273
furetur <furetur@gmail.com>
2020-11-05 12:16:14 +0300

    added config options

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,20 @@
 rootProject.name = "FightingGame"
-include("server", "shared")
+include("shared")
 
-val notIncludeAndroid = startParameter.projectProperties["notIncludeAndroid"]
+val localProperties = java.util.Properties()
+val localPropertiesFile = file("./local.properties")
 
-if (notIncludeAndroid != "true") {
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
+val notIncludeAndroid =
+    startParameter.projectProperties["notIncludeAndroid"] == "true" || localProperties["notIncludeAndroid"] == "true"
+if (!notIncludeAndroid) {
     include("android", "android:app")
+}
+
+val notIncludeServer = localProperties["notIncludeServer"] == "true"
+if (!notIncludeServer) {
+    include("server")
 }
