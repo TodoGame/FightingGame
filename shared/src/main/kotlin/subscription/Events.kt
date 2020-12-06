8860abf9b47731ecad0046ff5050953e9161c011
furetur <furetur@gmail.com>
2020-12-06 23:52:24 +0500

    added documentation

--- shared/src/main/kotlin/subscription/Events.kt
+++ shared/src/main/kotlin/subscription/Events.kt
@@ -18,16 +18,31 @@ sealed class SubscriptionMessage {
     abstract val subscribe: Boolean
 }
 
+/**
+ * Get notified every time users money updates
+ * Receive UserMoneyUpdate
+ */
 @Serializable
 data class UserMoneyUpdateSubscription(val username: Username, override val subscribe: Boolean = true) :
     SubscriptionMessage()
 
+/**
+ * Get notified every time leading faculty changes (some faculty takes over) or leading faculty's points update
+ * Receive LeadingFacultyUpdate
+ */
 @Serializable
 data class LeadingFacultySubscription(override val subscribe: Boolean = true) : SubscriptionMessage()
 
+/**
+ * Get notified every time any faculty's points are updated
+ * Receive FacultyPointsUpdate
+ */
 @Serializable
 data class AllFacultiesPointsSubscription(override val subscribe: Boolean = true) : SubscriptionMessage()
 
+/**
+ * Messages that the server sends every time something is updated
+ */
 @Serializable
 sealed class SubscriptionUpdate
 
